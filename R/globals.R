460767a4b8824bc57ba3b1479ed697e440310474
majazaloznik <maja.zaloznik@gmail.com>
2023-06-29 11:31:13 +0200

    Updated globals

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,6 @@
 utils::globalVariables(c(".", "name", "table_name", "interval", "all_same_interval",
-                         "table_code", "gr"))
+                         "table_code", "gr", "dimension_levels_code","count",
+                         "check_unique", "group_n", "duplicate_exists", "series_code" ))
 #' magrittr pipe
 #'
 #' @importFrom magrittr %>%
